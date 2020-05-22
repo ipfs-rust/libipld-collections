b677dd784971acd10bae204ef78fb3b8a9dd57db
David Craven <david@craven.ch>
2020-05-22 15:33:44 +0200

    Update deps.

--- examples/list_push.rs
+++ examples/list_push.rs
@@ -1,10 +1,12 @@
-use ipfs_embed::{Config, Store};
+//use ipfs_embed::{Config, Store};
+use libipld::mem::MemStore;
 use ipld_collections::List;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = Config::from_path("/tmp/list")?;
-    let store = Store::new(config)?;
+    //let config = Config::from_path("/tmp/list")?;
+    //let store = Store::new(config)?;
+    let store = MemStore::default();
 
     let mut list = List::new(store, 64, 256).await?;
     // push: 1024xi128; n: 4; width: 256; size: 4096
