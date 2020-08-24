00f1e3f9b00b6a99c41cbfe58a53ad49cbec10eb
dcraven <daniel@craven.ch>
2020-08-24 13:42:15 +0200

    Bump libipld.

--- examples/list_push.rs
+++ examples/list_push.rs
@@ -1,16 +1,17 @@
-//use ipfs_embed::{Config, Store};
 use ipld_collections::List;
+use libipld::cache::CacheConfig;
+use libipld::cbor::DagCborCodec;
+use libipld::error::Result;
 use libipld::mem::MemStore;
-use ipld_block_builder::{IpldCache, Codec};
+use libipld::multihash::Multihash;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //let config = Config::from_path("/tmp/list")?;
-    //let store = Store::new(config)?;
-    let store = MemStore::default();
-    let cache = IpldCache::new(store,Codec::new(),64);
-
-    let mut list = List::new(cache, 256).await?;
+    let store = MemStore::<DagCborCodec, Multihash>::new();
+    let mut config = CacheConfig::new(store, DagCborCodec);
+    config.size = 64;
+    let mut list = List::new(config, 256).await?;
     // push: 1024xi128; n: 4; width: 256; size: 4096
     for i in 0..1024 {
         list.push(i as i64).await?;
